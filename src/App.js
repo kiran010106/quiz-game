3dbb3ac2c3c51c3864bbd14f5b27994c7f9cbb4e
Kiran <kiran010106@gmail.com>
2023-02-10 18:03:05 +0530

    quiz game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Quiz from "./components/Quiz";
 
 function App() {
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+  const data = [
+    {
+      id: 1,
+      question:
+        "Rolex is the company that specialises in what type of product?",
+      answers: [
+        {
+          text: "Phone",
+          correct: false,
+        },
+        {
+          text: "Watches",
+          correct: true,
+        },
+        {
+          text: "Food",
+          correct: false,
+        },
+        {
+          text: "Cosmetics",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "When is Facebook is released?",
+      answers: [
+        {
+          text: "2005",
+          correct: false,
+        },
+        {
+          text: "2006",
+          correct: true,
+        },
+        {
+          text: "2007",
+          correct: false,
+        },
+        {
+          text: "2008",
+          correct: false,
+        },
+      ],
+    },
+
+    {
+      id: 3,
+      question: "Who played the character of harry potter in the movie?",
+      answers: [
+        {
+          text: "johnny deep",
+          correct: false,
+        },
+        {
+          text: "Denzel Washington",
+          correct: false,
+        },
+        {
+          text: "Daniel Redcliff",
+          correct: true,
+        },
+        {
+          text: "Taylor Swift",
+          correct: false,
+        },
+      ],
+    },
+  ];
+
+  const moneyPyramid = [
+    { id: 1, amount: "$ 100" },
+    { id: 2, amount: "$ 200" },
+    { id: 3, amount: "$ 300" },
+    { id: 4, amount: "$ 500" },
+    { id: 5, amount: "$ 1000" },
+    { id: 6, amount: "$ 2000" },
+    { id: 7, amount: "$ 4000" },
+    { id: 8, amount: "$ 8000" },
+    { id: 9, amount: "$ 16000" },
+    { id: 10, amount: "$ 320000" },
+    { id: 11, amount: "$ 640000" },
+    { id: 12, amount: "$ 125000" },
+    { id: 13, amount: "$ 250000" },
+    { id: 14, amount: "$ 500000" },
+    { id: 15, amount: "$ 1000000" },
+  ].reverse();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">
+          <Quiz
+            data={data}
+            setStop={setStop}
+            questionNumber = {questionNumber}
+            setQuestionNumber={setQuestionNumber}
+          />
+        </div>
+      </div>
+      <div className="pyramid">
+        <ul className="moneyList">
+          {moneyPyramid?.map((m , i) => (
+            <li
+              key={i}
+              className={
+                questionNumber === m.id
+                  ? "moneyListItem active"
+                  : "moneyListItem"
+              }
+            >
+              <span className="moneyListItemNumber">{m.id}</span>
+              <span className="moneyListItemAmount">{m.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
